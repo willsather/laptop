053284e78bd2a01e33b25843ea27e64730ebbd61
willsather <will@sather.ws>
2024-12-16 13:37:00 -0600

    feat: add zsh and oh-my-zsh

--- home/shell.nix
+++ home/shell.nix
@@ -1,10 +1,33 @@
-{...}: {
+{pkgs, ...}: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    shellAliases = {
+       ll = "ls -l";
+       lsa = "ls -la";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
+
     initExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
+
+    plugins = [
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.4.0";
+          sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+        };
+      }
+    ];
   };
 
   home.shellAliases = {
