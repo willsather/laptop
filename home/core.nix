b27d10677d761160ca16e6b788639146b2a7204f
willsather <will@sather.ws>
2024-12-17 10:35:02 -0600

    chore: add nix formatter

--- home/core.nix
+++ home/core.nix
@@ -1,32 +1,35 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     # archives
-    zip
-    xz
-    unzip
-    p7zip
+    zip # zip archiver
+    xz # compression tool
+    unzip # zip extractor
+    p7zip # 7z archiver
 
     # utils
-    jq # JSON processor
+    jq # json processor
     yq # yaml processer
     kubectl # kubernetes
     kubectx # kubernetes context
 
+    # nix
+    alejandra # formatter
+
     # misc
-    delta
-    curl
-    cowsay
-    httpie
-    htop
-    file
-    which
-    tree
-    gnused
-    gnutar
-    gawk
-    zstd
-    caddy
-    gnupg
+    delta # git diff viewer
+    curl # data transfer tool
+    cowsay # text art generator
+    httpie # http client tool
+    htop # process monitor
+    file # file type detector
+    which # command locator
+    tree # directory tree viewer
+    gnused # stream editor
+    gnutar # archive utility
+    gawk # text processing tool
+    zstd # compression tool
+    caddy # web server
+    gnupg # encryption tool
   ];
 
   programs = {
